2cce6639b4f9c1173116f045caa63f700560007b
mccordthomas <89097352+mccordthomas@users.noreply.github.com>
2022-07-22 00:28:45 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+def valid(row, col, board, i):
+    def cubes():
+        if row<=2 and col<=2:
+            return (i not in board[0:3, 0:3])
+        
+        elif row<=2 and 3<=col<=5:
+            return (i not in board[0:3, 3:6])  
+        
+        elif row<=2 and 6<=col<=8:
+            return (i not in board[0:3, 6:9])
+        
+        elif 3<=row<=5 and col<=2:
+            return (i not in board[3:6, 0:3])
+        
+        elif 3<=row<=5 and 3<=col<=5:
+            return (i not in board[3:6, 3:6])
+        
+        elif 3<=row<=5 and 6<=col<=8:
+            return (i not in board[3:6, 6:9])
+        
+        elif 6<=row<=8 and col<=2:
+            return (i not in board[6:9, 0:3])
+        
+        elif 6<=row<=8 and 3<=col<=5:
+            return (i not in board[6:9, 3:6])
+        
+        elif 6<=row<=8 and 6<=col<=8:
+            return (i not in board[6:9, 6:9])
+        
+    return (i not in board[row, ::] and i not in [r[col] for r in board] and cubes)      
+        
+
+def spot_is_zero(row, col, board):
+    return (board[row][col] == 0)
+
+
+def check_full(board):
+    for row in board:
+        if 0 not in row:
+            return True
+
+        
+def solve(row, col, board):
+    # check full board
+    if check_full(board):
+        return board
+
+    # boundary case
+    if col == len(board[row]):
+        row = row+1
+        col = 0
+    
+    # if open space
+    if spot_is_zero(row, col, board):
+        # check every number
+        for i in range(1,10):
+            if valid(row, col, board, i):
+                board[row][col] = i
+                if solve(row, col+1, board):
+                    return True
+        
+        # else backtrack
+        board[row][col] = 0
+        
+    else:
+        if col+1 > 8:
+            solve(row=row+1, col=0, board=board)
+        else:
+            solve(row, col+1, board)
+
+
+def sudoku(board):
+    print(solve(0, 0, board))
